1589b8b4f409e572d73258f01e848c3bc7451f06
alvarobc2412 <alvarobc2412@gmail.com>
2023-10-24 10:22:30 +0000

    fix: Delete census changes

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -4,11 +4,13 @@ from .models import Census
 
 
 class CensusAdmin(admin.ModelAdmin):
-    list_display = ('voting_id', 'voter_id','adscription')
-    list_filter = ('voting_id','adscription' )
+    list_display = ('voting_id', 'voter_id')
+    # list_display = ('voting_id', 'voter_id','adscription')
+    list_filter = ('voting_id', )
+    # list_filter = ('voting_id', 'adscription' )
     search_fields = ('voter_id', )
     
-    def adscription(self, obj):
-        return obj.adscription
+    # def adscription(self, obj):
+    #     return obj.adscription
 
 admin.site.register(Census, CensusAdmin)
